0bc9451b2c987352bb27c75c7346e1e9d79a1ddb
Ilya <60040129+mrProger@users.noreply.github.com>
2022-09-23 18:03:57 +0600

    Правка System::isNull($value) в \PHPSystem\System: Вырезана проверка на пустоту строки

--- app/PHPSystem/System.php
+++ app/PHPSystem/System.php
@@ -5,7 +5,7 @@ namespace PHPSystem;
 class System {
     public static function isNull($value) {
         if (gettype($value) == "string") {
-            return $value == null || strtolower($value) == 'null' || strlen(trim($value)) == 0;
+            return $value == null || strtolower($value) == 'null';
         } else {
             return $value == null;
         }
