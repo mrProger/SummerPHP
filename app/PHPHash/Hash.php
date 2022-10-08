73d74f6a05f8374bbfe5ac9501f1a789b7c438e3
Ilya Khrebtov <zkhrebtovilya@gmail.com>
2022-10-08 12:46:18 +0600

    Реализована бибилиотека для хеширования данных

---
+++ app/PHPHash/Hash.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PHPHash;
+
+include __DIR__ . '/../PHPSystem/System.php';
+
+use \PHPSystem\System;
+use \PHPExceptionHandler\ExceptionHandler;
+
+class Hash {
+    public static function sha256(string $value, string $salt, int $count) {
+        if ($count <= 0) {
+            return;
+        }
+
+        if (System::isNull($value)) {
+            ExceptionHandler::generateError("Невозможно сгенерировать хеш из пустого значения");
+        }
+
+
+        $hash = $value;
+
+        if (!System::isNull($salt)) {
+            $hash .= $salt;
+        }
+
+        for ($i = 0; $i < $count; $i++) {
+            $hash = hash("sha256", $hash);
+        }
+
+        return $hash;
+    }
+
+    public static function md5(string $value, string $salt, int $count) {
+        if ($count <= 0) {
+            return;
+        }
+
+        if (System::isNull($value)) {
+            ExceptionHandler::generateError("Невозможно сгенерировать хеш из пустого значения");
+        }
+
+
+        $hash = $value;
+
+        if (!System::isNull($salt)) {
+            $hash .= $salt;
+        }
+
+        for ($i = 0; $i < $count; $i++) {
+            $hash = hash("md5", $hash);
+        }
+
+        return $hash;
+    }
+}
